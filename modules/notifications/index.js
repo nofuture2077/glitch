c1fd47ecb048fbf0217caa9416ce211e8c3bc7d5
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2023-04-07 00:28:03 +0200

    include google cloud ttts

--- modules/notifications/index.js
+++ modules/notifications/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const PubSub = require('pubsub-js');
 var fs = require('fs');
 
 const queue = [];
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 module.exports = function(options) {
 
@@ -12,14 +14,16 @@ module.exports = function(options) {
     });
 
     PubSub.subscribe('MSG!tts', (message, data) => {
+        const text = data.parts.slice(1).join(" ");
+        if (!text.trim()) return;
         PubSub.publish('notifications', {
             type: 'tts',
-            user: data.displayName,
+            user: data.displayname,
             text: data.parts.slice(1).join(" ")
         });
     });
     
-    var html = fs.readFileSync('./modules/notifications/notifications.html', 'utf8');
+    var html = fs.readFileSync('./modules/notifications/notifications.html', 'utf8').replace('GOOGLE_API_KEY', GOOGLE_API_KEY);
 
     return {
         html
